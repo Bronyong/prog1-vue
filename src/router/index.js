9f11e2225bc1a50a190cac4f30b321e1928a2d69
Bronyong <sulthania30@gmail.com>
2023-05-08 14:53:25 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Homepage',
+    component: () => import('../views/HomePage.vue')
+  },
+  {
+    path: '/',
+    component: () => import('../views/dashboard/LayoutAdmin.vue'),
+    children: [
+      {
+        path: '/dashboard',
+        component: () => import('../views/dashboard/Dashboard.vue')
+      }
+    ]
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/dashboard/Dashboard.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci neque harum quidem voluptate sed natus omnis commodi libero distinctio deserunt delectus explicabo odio nulla aspernatur, culpa ea architecto veritatis aperiam.</h4>
+    </div>
+</template>
